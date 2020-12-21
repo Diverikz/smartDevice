e771116092232ee2b9f797e9580fba35d7a653b0
Diverikz <vidas.m@mail.ru>
2020-12-21 12:55:57 +0300

    do adaptive

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ document.querySelector(".close").addEventListener("click", function () {
   document.querySelector(".modal").style.display = "none";
   document.body.style.overflow = "visible";
 });
+
+function mobileNavAdjustment() {
+  if ($(window).width() < 610) {
+    var navWidth = $("nav").outerWidth();
+    var logoWidth = $(".small-logo").outerWidth();
+    var menuLinesWidth = $(".menu__lines").outerWidth(true);
+    var mailIconWidth = $("#mail-icon").outerWidth(true);
+    var phoneIconWidth = $("#phone-icon").outerWidth(true);
+    var marg =
+      navWidth -
+      (logoWidth + menuLinesWidth + mailIconWidth + phoneIconWidth + 20);
+    $(".small-logo").css("margin-right", marg);
+  }
+}
